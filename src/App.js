d9bda26a96408660f6bf776fb779f74851bf1ea9
iliamak <54379289+iliamak@users.noreply.github.com>
2025-02-25 12:50:58 +0500

    Добавление компонента App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PersonalBlog from './components/PersonalBlog';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonalBlog />
+    </div>
+  );
+}
+
+export default App;
